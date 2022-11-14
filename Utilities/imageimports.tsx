bd6e432d4b94a789be202bcce46d73cadcf39d94
JLHarris-Vectorform <111083579+Jharris-vectorform@users.noreply.github.com>
2022-11-14 09:13:40 -0600

    Create imageimports.tsx

---
+++ Utilities/imageimports.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+
+import logo from "../assets/MP_logo_horizontal_1.svg";
+import home from "../assets/home.svg";
+import homeBlue from "../assets/home_blue.svg";
+import plant from "../assets/factory-grey.svg";
+import plantBlue from "../assets/plant_info_blue.svg";
+import documentWarn from "../assets/document-warning-1.svg";
+import documentWarnBlue from "../assets/issue_report_blue.svg";
+import partsNservices from "../assets/parts-services-1.svg";
+import documentSearch from "../assets/document-search.svg";
+import support from "../assets/support.svg";
+import settings from "../assets/settings.svg";
+import menuOpen from "../assets/arrow-right.svg";
+import openClose from "../assets/nav_icon.svg";
+import magnifyGlass from "../assets/search-loupe.svg";
+import magnifyGlassBlack from "../assets/magnifyGlass_black.svg";
+import menuCloseIcon from "../assets/close-menu.svg";
+import bellIcon from "../assets/grey-bell-icon.svg";
+import menuOpenIcon from "../assets/open-menu.svg";
+import alarmWarning from "../assets/alarm-warning.svg";
+import alarmWarningBlack from "../assets/alarm-warning-black.svg";
+import linkout from "../assets/linkout.svg";
+import downArrow from '../assets/arrow-down.svg';
+import gpsPin from '../assets/gps-pin.svg'
+import factory from '../assets/plant.svg';
+import pencil from "../assets/pencil.svg";
+import pencilWhite from "../assets/editWhite.svg";
+import tooltip from '../assets/Tool-Tip-icon.svg';
+import closeImg from '../assets/close.svg';
+import closeError from '../assets/close_error.svg';
+import dangerIcon from '../assets/danger_icon.svg';
+import filter from '../assets/filter.svg';
+import gear from '../assets/gear.svg';
+import SteamIsmg from "../assets/temp/steam-img.png";
+import nameplate_attachmentIMG from "../assets/temp/nameplate_attachment.jpg";
+import SolarIsmg from "../assets/temp/solar-img.png";
+import ellispis from "../assets/3_dots_horizontal.svg";
+import Arrow from "../assets/arrow-right.svg";
+import trash from '../assets/trash.svg';
+import file_edit from '../assets/file_edit.svg';
+import download from '../assets/download.svg';
+import print from '../assets/print.svg';
+import dragReorder from '../assets/drag_reorder.svg';
+import fileTab from "../assets/file-tab.svg"; 
+import arrowDown from "../assets/accordian-down.svg"; 
+import avatar from "../assets/temp/Avatar.png";
+import spinner from "../assets/loader.gif";
+import add from "../assets/add.svg";
+import userGroup from '../assets/userGroup.svg';
+import attachment from '../assets/attachment.svg';
+import copyLink from '../assets/copy_link.svg';
+import info from '../assets/info.svg';
+import success from '../assets/success.svg';
+import blueTick from '../assets/blue_tick.svg';
+import help from '../assets/help.svg';
+import remoteMonitoring from '../assets/remote-monitoring.svg';
+import gearTime from '../assets/gear-time.svg';
+import userGroup2 from '../assets/user-group.svg';
+import userGroupIcon from "../assets/user-group-icon.svg";
+import documentIcon from "../assets/document.svg";
+import attachmentIcon from "../assets/attachment.svg";
+import copy_link from "../assets/copy_link.svg";
+import privacy_icon from "../assets/privacy-icon.svg";
+import checked_icon from "../assets/checked_icon.svg";
+import partial_checked_icon from "../assets/partial_checked_icon.svg";
+import checked_disabled_icon from "../assets/checked_disabled_icon.svg";
+
+
+const ImageImports = {
+  checked_icon,
+  partial_checked_icon,
+  checked_disabled_icon,
+  privacy_icon,
+  alarmWarningBlack,
+  blueTick,
+  success,
+  info,
+  add,
+  spinner,
+  avatar,
+  arrowDown,
+  fileTab,
+  nameplate_attachmentIMG,
+  print,
+  download,
+  file_edit,
+  trash,
+  Arrow,
+  ellispis,
+  SolarIsmg,
+  SteamIsmg,
+  closeError,
+  dangerIcon,
+  pencilWhite,
+  pencil,
+  factory,
+  gpsPin,
+  downArrow,
+  linkout,
+  alarmWarning,
+  menuOpenIcon,
+  bellIcon,
+  menuCloseIcon,
+  logo,
+  tooltip,
+  home,
+  closeImg,
+  homeBlue,
+  plant,
+  plantBlue,
+  documentWarn,
+  documentWarnBlue,
+  partsNservices,
+  documentSearch,
+  support,
+  settings,
+  menuOpen,
+  openClose,
+  magnifyGlass,
+  magnifyGlassBlack,
+  filter,
+  gear,
+  dragReorder,
+  userGroup,
+  attachment,
+  copyLink,
+  help,
+  remoteMonitoring,
+  gearTime,
+  userGroup2,
+  userGroupIcon,
+  documentIcon,
+  attachmentIcon,
+  copy_link
+}
+export default ImageImports
